72137f07092b6cc2376f0fed78753c715ec0d5b4
shuye <804745459@qq.com>
2017-01-04 09:22:29 +0800

    初始化

---
+++ spring-cloud-starter-config/src/main/java/com/spring/cloud/service/Application.java
@@ -0,0 +1,11 @@
+package com.spring.cloud.service;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+}
