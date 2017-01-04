d41b9d318f18198ce9d924afa24e91c5689e1993
shuye <804745459@qq.com>
2017-01-04 17:30:23 +0800

    初始化-添加服务集群2

---
+++ spring-cloud-service2/src/main/java/com/spring/cloud/service/ComputeServiceApplication.java
@@ -0,0 +1,15 @@
+package com.spring.cloud.service;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ComputeServiceApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ComputeServiceApplication.class).web(true).run(args);
+    }
+
+}
