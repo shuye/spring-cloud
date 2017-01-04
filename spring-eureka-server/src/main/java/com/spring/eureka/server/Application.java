17a782a7b370f4fe5fa4186a01ab67addfe52ed9
shuye <804745459@qq.com>
2017-01-04 09:16:08 +0800

    初始化

---
+++ spring-eureka-server/src/main/java/com/spring/eureka/server/Application.java
@@ -0,0 +1,16 @@
+package com.spring.eureka.server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+}
